5cd6d44209a867a4df9e27d6755f0ebdb4b25b91
nadiaGueta <segma55@outlook.com>
2026-09-23 21:37:38 +0200

    TDD green : add 2 green  tests for US-1

--- src/test/java/devis/LignePieceTest.java
+++ src/test/java/devis/LignePieceTest.java
@@ -1,6 +1,8 @@
 package devis;
 
+import garage.app.devis.LignePiece;
 import garage.domain.Montant;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 public class LignePieceTest {
@@ -8,6 +10,7 @@ public class LignePieceTest {
 
 
     @Test
+    @DisplayName("Quatre plaquettes à 43,90 € coûtent 175,60 €")
     void quatre_plaquettes_a_43_90_coutent_175_60_euros() {
         LignePiece ligne = new LignePiece(
                 "Plaquette",
