2fb5953153cc9d0c57dc40888804e1905ff41162
MSI <yassinmoujahid46@gmail.com>
2026-09-23 18:43:17 +0200

    test: choose earliest slot across compatible bays

--- src/test/java/planning/PlanningTest.java
+++ src/test/java/planning/PlanningTest.java
@@ -221,4 +221,211 @@ class PlanningTest {
                 resultat.creneau()
         );
     }
+
+    @Test
+    void doit_trouver_le_premier_creneau_libre_sur_une_baie_occupee() {
+
+        Vehicule vehicule = new Vehicule(
+                "AA-123-AA",
+                "Peugeot 208",
+                1400
+        );
+
+        Operation operation = new Operation(
+                Atelier.MECANIQUE,
+                Duration.ofHours(2)
+        );
+
+        Dossier dossier = new Dossier(
+                new Client("Yassine"),
+                vehicule,
+                List.of(operation)
+        );
+
+        Baie pont25 = Baie.pont(
+                2500,
+                new Creneau(
+                        LocalTime.of(8, 0),
+                        LocalTime.of(10, 0)
+                )
+        );
+
+        Planning planning = new Planning(pont25);
+
+        Planning.Resultat resultat =
+                planning.chercherCreneau(
+                        dossier,
+                        LocalTime.of(8, 0)
+                );
+
+        assertTrue(resultat.estDisponible());
+
+        assertSame(
+                pont25,
+                resultat.baie()
+        );
+
+        assertEquals(
+                new Creneau(
+                        LocalTime.of(10, 0),
+                        LocalTime.of(12, 0)
+                ),
+                resultat.creneau()
+        );
+    }
+
+    @Test
+    void doit_ignorer_une_baie_trop_petite_et_choisir_la_suivante() {
+
+        Vehicule vehicule = new Vehicule(
+                "AA-123-AA",
+                "Peugeot 308",
+                3200
+        );
+
+        Operation operation = new Operation(
+                Atelier.MECANIQUE,
+                Duration.ofHours(2)
+        );
+
+        Dossier dossier = new Dossier(
+                new Client("Yassine"),
+                vehicule,
+                List.of(operation)
+        );
+
+        Baie pont25 = Baie.pont(2500);
+        Baie pont35 = Baie.pont(3500);
+
+        Planning planning = new Planning(
+                pont25,
+                pont35
+        );
+
+        Planning.Resultat resultat =
+                planning.chercherCreneau(
+                        dossier,
+                        LocalTime.of(8, 0)
+                );
+
+        assertTrue(resultat.estDisponible());
+
+        assertSame(
+                pont35,
+                resultat.baie()
+        );
+
+        assertEquals(
+                new Creneau(
+                        LocalTime.of(8, 0),
+                        LocalTime.of(10, 0)
+                ),
+                resultat.creneau()
+        );
+    }
+
+    @Test
+    void doit_refuser_si_aucune_baie_ne_permet_un_creneau_de_trois_heures() {
+
+        Vehicule vehicule = new Vehicule(
+                "AA-123-AA",
+                "Peugeot 308",
+                1400
+        );
+
+        Operation operation = new Operation(
+                Atelier.MECANIQUE,
+                Duration.ofHours(3)
+        );
+
+        Dossier dossier = new Dossier(
+                new Client("Yassine"),
+                vehicule,
+                List.of(operation)
+        );
+
+        Baie pont25 = Baie.pont(
+                2500,
+                new Creneau(
+                        LocalTime.of(8, 0),
+                        LocalTime.of(12, 0)
+                ),
+                new Creneau(
+                        LocalTime.of(14, 0),
+                        LocalTime.of(18, 0)
+                )
+        );
+
+        Planning planning = new Planning(pont25);
+
+        Planning.Resultat resultat =
+                planning.chercherCreneau(
+                        dossier,
+                        LocalTime.of(8, 0)
+                );
+
+        assertFalse(resultat.estDisponible());
+
+        assertEquals(
+                "Aucun créneau compatible disponible aujourd'hui",
+                resultat.raison()
+        );
+    }
+
+    @Test
+    void doit_choisir_le_creneau_le_plus_tot_parmi_les_baies_compatibles() {
+
+        Vehicule vehicule = new Vehicule(
+                "AA-123-AA",
+                "Peugeot 308",
+                1400
+        );
+
+        Operation operation = new Operation(
+                Atelier.MECANIQUE,
+                Duration.ofHours(3)
+        );
+
+        Dossier dossier = new Dossier(
+                new Client("Yassine"),
+                vehicule,
+                List.of(operation)
+        );
+
+        Baie pont25 = Baie.pont(
+                2500,
+                new Creneau(
+                        LocalTime.of(8, 0),
+                        LocalTime.of(14, 0)
+                )
+        );
+
+        Baie pont35 = Baie.pont(3500);
+
+        Planning planning = new Planning(
+                pont25,
+                pont35
+        );
+
+        Planning.Resultat resultat =
+                planning.chercherCreneau(
+                        dossier,
+                        LocalTime.of(8, 0)
+                );
+
+        assertTrue(resultat.estDisponible());
+
+        assertSame(
+                pont35,
+                resultat.baie()
+        );
+
+        assertEquals(
+                new Creneau(
+                        LocalTime.of(8, 0),
+                        LocalTime.of(11, 0)
+                ),
+                resultat.creneau()
+        );
+    }
 }
