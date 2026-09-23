cab40e4959d65ef72867a9038f6cd23173ed7d60
MSI <yassinmoujahid46@gmail.com>
2026-09-23 14:05:36 +0200

    feat(planning): verifier les horaires d'ouverture

--- src/main/java/garage/domain/planning/Creneau.java
+++ src/main/java/garage/domain/planning/Creneau.java
@@ -16,4 +16,14 @@ public class Creneau {
         return this.fin.compareTo(occupe.debut) <= 0
                 || this.debut.compareTo(occupe.fin) >= 0;
     }
+
+    public boolean estDansLesHorairesDuGarage() {
+        boolean matin = !debut.isBefore(LocalTime.of(8, 0))
+                && !fin.isAfter(LocalTime.of(12, 0));
+
+        boolean apresMidi = !debut.isBefore(LocalTime.of(14, 0))
+                && !fin.isAfter(LocalTime.of(18, 0));
+
+        return matin || apresMidi;
+    }
 }
