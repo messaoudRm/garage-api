6b83235480d4397173bd19ff5472446ea335f309
nadiaGueta <segma55@outlook.com>
2026-09-24 10:09:01 +0200

    TDD REFACTOR US-2: mutualiser la tarification par quart d'heure DANS MainOeuvreMecanique

--- src/main/java/garage/app/devis/MainOeuvreMecanique.java
+++ src/main/java/garage/app/devis/MainOeuvreMecanique.java
@@ -4,19 +4,13 @@ import garage.domain.Montant;
 
 import java.time.Duration;
 
-public class MainOeuvreMecanique {
+public class MainOeuvreMecanique extends TarificationParQuart {
 
-    private static final long PRIX_QUART_HEURE = 1550;
+    public MainOeuvreMecanique() {
+        super(1550);
+    }
 
-
-    public Montant totalMainOeuvre(Duration duree){
-long minutes = duree.toMinutes() ;
-
-        long nombreDeQuarts = minutes / 15;
-
-        if (minutes % 15 != 0) {
-            nombreDeQuarts++;}
-
-return Montant.centimes( nombreDeQuarts * PRIX_QUART_HEURE) ;
+    public Montant totalMainOeuvre(Duration duree) {
+        return calculer(duree);
     }
 }
