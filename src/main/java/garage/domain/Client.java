face7837cbfbd9ac0b1e4e0f95855c828e859f7d
Boubouss <redwanbsm@gmail.com>
2026-09-25 10:27:32 +0200

    US-8 : ajout de la modification pour Client et constructeur secondaire

--- src/main/java/garage/domain/Client.java
+++ src/main/java/garage/domain/Client.java
@@ -1,4 +1,7 @@
 package garage.domain;
 
-public record Client(String nom) {}
-
+public record Client(String nom, boolean professionnel, boolean habitue) {
+  public Client(String nom) {
+    this(nom, false, false);
+  }
+}
